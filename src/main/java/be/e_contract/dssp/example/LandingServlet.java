296df9822e07befe4ab5e56d3778b5292dc37a8f
Frank Cornelis <info@e-contract.be>
2020-05-19 09:51:31 +0200

    payara example runtime

--- src/main/java/be/e_contract/dssp/example/LandingServlet.java
+++ src/main/java/be/e_contract/dssp/example/LandingServlet.java
@@ -3,12 +3,16 @@ package be.e_contract.dssp.example;
 import be.e_contract.dssp.client.DigitalSignatureServiceClient;
 import be.e_contract.dssp.client.DigitalSignatureServiceSession;
 import be.e_contract.dssp.client.SignResponseVerifier;
-import be.e_contract.dssp.client.SignatureInfo;
 import be.e_contract.dssp.client.VerificationResult;
 import be.e_contract.dssp.client.exception.AuthenticationRequiredException;
+import be.e_contract.dssp.client.exception.ClientRuntimeException;
 import be.e_contract.dssp.client.exception.DocumentSignatureException;
+import be.e_contract.dssp.client.exception.KeyLookupException;
+import be.e_contract.dssp.client.exception.SubjectNotAuthorizedException;
 import be.e_contract.dssp.client.exception.UnknownDocumentException;
 import be.e_contract.dssp.client.exception.UnsupportedDocumentTypeException;
+import be.e_contract.dssp.client.exception.UserCancelException;
+import be.e_contract.dssp.client.spi.Base64DecodingException;
 import java.io.IOException;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -17,8 +21,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.bind.JAXBException;
+import javax.xml.crypto.MarshalException;
+import javax.xml.crypto.dsig.XMLSignatureException;
+import javax.xml.parsers.ParserConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
 @WebServlet("/landing")
 public class LandingServlet extends HttpServlet {
@@ -35,7 +44,9 @@ public class LandingServlet extends HttpServlet {
         String signResponse = request.getParameter("SignResponse");
         try {
             SignResponseVerifier.checkSignResponse(signResponse, session);
-        } catch (Exception ex) {
+        } catch (ClientRuntimeException | KeyLookupException | SubjectNotAuthorizedException | UserCancelException
+                | Base64DecodingException | IOException | JAXBException
+                | MarshalException | XMLSignatureException | ParserConfigurationException | SAXException ex) {
             LOGGER.error("error: " + ex.getMessage(), ex);
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "error verifying response: " + ex.getMessage());
             return;
@@ -47,6 +58,7 @@ public class LandingServlet extends HttpServlet {
             signedDocument = client.downloadSignedDocument(session);
         } catch (UnknownDocumentException ex) {
             LOGGER.error("error: " + ex.getMessage(), ex);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "error downloading signed document: " + ex.getMessage());
             return;
         }
         VerificationResult verificationResult;
@@ -54,6 +66,7 @@ public class LandingServlet extends HttpServlet {
             verificationResult = client.verify("application/pdf", signedDocument);
         } catch (UnsupportedDocumentTypeException | DocumentSignatureException | AuthenticationRequiredException ex) {
             LOGGER.error("error: " + ex.getMessage(), ex);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "error verifying signed document: " + ex.getMessage());
             return;
         }
         this.demoController.setVerificationResult(verificationResult);
